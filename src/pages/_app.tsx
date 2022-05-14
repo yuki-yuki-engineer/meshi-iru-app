d05e42a6c7d226304469c10b15815d063317c53e
yuki-kawachi777 <yuki.kawachi@atrae.co.jp>
2022-05-14 14:00:15 +0900

    feat: chakra-uiの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import "../../styles/globals.css";
+import { theme } from "../../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} theme={theme} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#319795",
+    800: "#37aaa8",
+    700: "#3dbebb",
+  },
+};
+
+export const theme = extendTheme({ colors });
